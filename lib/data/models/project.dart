bec1b2dc5e7dde93688522603332cfe84642d978
Yczar <babalolagbogo@gmail.com>
2022-10-24 12:22:52 +0100

    :beers: created project model

---
+++ lib/data/models/project.dart
@@ -0,0 +1,35 @@
+// import 'package:equatable/equatable.dart';
+
+// class Project extends Equatable {
+//   const Project({
+//     this.id,
+//     this.name,
+//     this.description,
+//     this.createdAt,
+//     this.updatedAt,
+//   });
+//   final String? id;
+//   final String? name;
+//   final String? description;
+//   final String? createdAt;
+//   final String? updatedAt;
+
+//   Project copyWith({
+//     String? id,
+//     String? name,
+//     String? description,
+//     String? createdAt,
+//     String? updatedAt,
+//   }) {
+//     return Project(
+//       id: id ?? this.id,
+//       name: name ?? this.name,
+//       description: description ?? this.description,
+//       createdAt: createdAt ?? this.createdAt,
+//       updatedAt: updatedAt ?? this.updatedAt,
+//     );
+//   }
+
+//   @override
+//   List<Object?> get props => [id, name, description, createdAt, updatedAt];
+// }
