b8c9dfdc4129b31eba7bf98f0ebf14886626e98d
Yczar <babalolagbogo@gmail.com>
2022-10-26 01:33:58 +0100

    :sparkles: modified: the bearer auth middleware

--- lib/middlewares/bearer_auth_middleware.dart
+++ lib/middlewares/bearer_auth_middleware.dart
@@ -1,14 +1,18 @@
 // ignore_for_file: public_member_api_docs
 
+import 'dart:io';
+
 import 'package:dart_frog/dart_frog.dart';
 import 'package:team_ship_dart_frog/data/models/client_error.dart';
+import 'package:team_ship_dart_frog/data/models/user.dart';
 import 'package:team_ship_dart_frog/server/db_connection.dart';
+import 'package:team_ship_dart_frog/services/github_service.dart';
 import 'package:team_ship_dart_frog/utils/constants/api_constants.dart';
 import 'package:team_ship_dart_frog/utils/constants/db_constants.dart';
 
 import 'package:team_ship_dart_frog/utils/helpers/app_helper.dart';
 
-Middleware get bearerAuthMiddleware => (handler) {
+Middleware get bearerAuthMiddleware => (Handler handler) {
       return (context) async {
         final api = context.request.uri.pathSegments.last;
         if (ignoreAuthorization.contains(api)) {
@@ -36,12 +40,35 @@ Middleware get bearerAuthMiddleware => (handler) {
             ).toJson(),
           );
         } else {
+          final user = User.fromMap(userMap);
           final verifyToken = await AppHelper.verifyToken(token, collection);
+          final verifyGithub =
+              await GithubService.instance.verifyGithubUsername(
+            user.githubUsername ?? '',
+            // 'Yczar',
+          );
+          if (!verifyGithub) {
+            return Response(
+              statusCode: HttpStatus.unauthorized,
+              body: ClientError(
+                message:
+                    'There seems to be a problem with your github account. '
+                    'Please update your github username in your profile.',
+              ).toJson(),
+            );
+          }
           if (verifyToken) {
-            return await handler(context);
+            return await handler
+                .use(provider<Token>((context) => token))
+                .use(
+                  provider<User>(
+                    (context) => user,
+                  ),
+                )
+                .call(context);
           } else {
             return Response(
-              statusCode: 401,
+              statusCode: HttpStatus.unauthorized,
               body: ClientError(
                 message: 'Unauthorized',
               ).toJson(),
@@ -50,3 +77,5 @@ Middleware get bearerAuthMiddleware => (handler) {
         }
       };
     };
+
+typedef Token = String;
